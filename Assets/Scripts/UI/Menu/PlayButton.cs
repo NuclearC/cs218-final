60e553990d61e353a4fbc4099a719cc1498125bd
NuclearC <armenberug@gmail.com>
2025-05-04 20:26:08 +0400

    Fix settings scene

--- Assets/Scripts/UI/Menu/PlayButton.cs
+++ Assets/Scripts/UI/Menu/PlayButton.cs
@@ -1,14 +1,18 @@
 
+
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
 
 public class PlayButton : MonoBehaviour
 {
+    [SerializeField] Image backgroundImage;
     public void ChangeScene()
     {
+        backgroundImage.color = new Color(0, 0, 0.5f);
 
         var transition = FindObjectOfType<TransitionHelper>();
         transition.TransitionTo(2);
